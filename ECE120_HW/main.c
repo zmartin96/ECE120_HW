1e76f458d2e6cc9ecfe7d29efff50a96d9c8aa6d
zmartin96 <zachary1131@gmail.com>
2023-09-11 12:05:12 -0500

    Initial Commit

---
+++ ECE120_HW/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ECE120_HW
+//
+//  Created by Zachary Martin on 9/11/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}

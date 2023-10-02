f39cc93b9596d67123010b9cfd80ea7ea7bcd52a
zmartin96 <zachary1131@gmail.com>
2023-10-02 15:38:51 -0500

    HW #6

--- ECE120_HW/K-Map.c
+++ ECE120_HW/K-Map.c
@@ -1,36 +1,79 @@
 #include <stdio.h>
 
+unsigned int testFunction(unsigned int *function,
+						  unsigned int*w,unsigned int*x,
+						  unsigned int*y,unsigned int*z)
+{
+	if (*function==0)
+	{
+		// f(w,x,y,z) = w'x' + x'y' + y'z + z'w'
+		return ((!*w&&!*x)||(!*x&&!*y)||(!*y&&*z)||(!*z&&!*w));
+	}
+	else
+	{
+		// g(w,x,y,z) = (x+y)(w'+y+z')
+		return ((*x||*y)&&(!*w||*y||!*z));
+	}
+};
+
 int main(void)
 {
-	unsigned int a, b, c, d;
+	// Hey Jonathan, have a nice day!
+	unsigned int a, b, c, d, w, x, y, z;
+	// definition of functions
+	char funct[2][40]={"f(w,x,y,z) = w'x' + x'y' + y'z + z'w'",
+						"g(w,x,y,z) = (x+y)(w'+y+z')"};
 	unsigned int f;
-	/* Print header for K-map. */
-	printf("\t\t  bc \n"); // I did change some of the spacing to make it look a little nicer
-	printf("\t  00 01 11 10 \n");
-	printf("\t _______________\n");
-	/* row-printing loop */
-	for (a = 0; 2 > a; a=a+1) {
-		printf("a=%u | ", a);
-		/* Loop over input variable b in binary order. */
-		for (b = 0; 2 > b; b=b+1) {
-			/* Loop over d in binary order.*/
-			for (d = 0; 2 > d; d=d+1)
+	
+	// iterates a, which picks the function (f or g)
+	for (a=0;2>a;a++)
+	{
+		printf("Function %s:\n\n",*(funct+a));
+		/* Print header for K-map. */
+		printf("\t\t\t wx \n");
+		printf("\t\t00 01 11 10 \n");
+		printf("\t   _______________\n");
+		
+		// iterate y, where binary order=Gray code order
+		for (y = 0; 2>y; y++) 
+		{
+			/* Loop over input variable b in binary order. */
+			for (b = 0; 2 > b; b++) 
 			{
-				/* Use variables b and d to calculate *
-				 * input variable c (iterated in *
+				/* Use variables b and y to calculate *
+				 * input variable z (iterated in *
 				 * Gray code order). */
-				c=(!b&&d)||(b&&!d);
-				/* Calculate and print one K-map entry *
-				 * (function F(a,b,c) ). */
+				z=(!b&&y)||(b&&!y);
 				
-				// F(a,b,c) = (a+b+c’)(a’+b)(b’+c’)
-				f=(a||b||!c)&&(!a||b)&&(!b||!c);
+				// print the vertical axis labels
+				printf("yz=%u%u | ", y,z);
 				
-				printf(" %d ", f);
+				// iterate w, where binary order=Gray code order
+				for (w=0;2>w;w++)
+				{
+					/* Loop over d in binary order.*/
+					for (d=0;2>d;d++)
+					{
+						/* Use variables w and d to calculate *
+						 * input variable x (iterated in *
+						 * Gray code order). */
+						x=(!w&&d)||(w&&!d);
+						
+						// call the function to calculate K-map entry
+						f=testFunction(&a, &w, &x, &y, &z);
+						
+						// print resulting value
+						printf(" %d ", f);
+					}
+					
+				}
+				/* End of row reached: print a newline character. */
+				printf("\n");
 			}
+			
 		}
-		/* End of row reached: print a newline character. */
-		printf("\n");
+		// End of function reached: two newlines for visual separation
+		printf("\n\n");
 	}
 	return 0;
 }
